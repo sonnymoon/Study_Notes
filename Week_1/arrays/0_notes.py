f0d4787283c7c3290965270c015c81e2156d07af
sonnymoon <symoon654@gmail.com>
2023-04-01 01:31:14 -0400

    refactor

--- Week_1/arrays/0_notes.py
+++ Week_1/arrays/0_notes.py
@@ -1,4 +1,5 @@
-##### Arrays #####
+# https://leetcode.com/explore/learn/card/fun-with-arrays/
+# https://leetcode.com/explore/learn/card/array-and-string/
 
 # An array is a collection of items or "elements"
 # An array can be described by its length (the number of elements it contains) as well as its capacity (the number of elements it is able to contain)
@@ -81,18 +82,3 @@ def array_testing():
 		print('The element was not found in the array')
 
 	print('AFTER', length, array)
-
-# Unicode (Universal Character Set) is a set of characters that comprises of formulas, symbols, and text of various languages
-# There are multiple unicode encoding schemes to represent characters in 7, 8, 16, or 32 bits
-
-# Another set of characters is ASCII (American Standard Code for Information Interchange) which does not encompass as wide a range of characters as Unicode
-# The ASCII encoding schemes only represent characters in 7 or 8 bits and as such, ASCII is a subset of Unicode
-
-# Unicode is used more often for encoding and representing characters in software
-# ASCII is used for electronic communication and programming languages like HTML
-
-# A string is an array of unicode characters
-# As such, many array operations can also be applied to strings, but there are some that behave differently depending on the language
-    # The "==" operator may or may not do a proper comparison of whether two strings are the same in composition
-    # Strings are immutable in some languages, meaning that they cannot be changed (i.e. changing the character at an index)
-
