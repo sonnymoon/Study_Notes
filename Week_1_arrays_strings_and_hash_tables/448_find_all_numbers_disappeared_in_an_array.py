d559d399f1bd2902f8ad290689cdd00f9d67140c
sonnymoon <symoon654@gmail.com>
2023-04-05 00:11:55 -0400

    explanations

--- Week_1_arrays_strings_and_hash_tables/448_find_all_numbers_disappeared_in_an_array.py
+++ Week_1_arrays_strings_and_hash_tables/448_find_all_numbers_disappeared_in_an_array.py
@@ -10,3 +10,9 @@ class Solution(object):
 		
 		return [ i + 1 for i in range(len(nums)) if nums[i] > 0 ]
 
+# Explanation
+# Each number in the array is iterated through using a for loop
+	# A variable "index" is assigned to the absolute value of the number minus 1
+		# The indexes of the array themselves are used to mark whether the number has been seen in the array
+	# The number at "index" is reassigned to the negative absolute value of that number
+# The function returns an array of all the indexes of the array plus 1 where the number at that index is positive
